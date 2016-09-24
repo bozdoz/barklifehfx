3a85e6abea785dc64b740ef6724b6e82bebadeae
bozdoz <howaboutben@gmail.com>
2016-09-24 00:04:06 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+    return browserSync.init({
+        proxy : '127.0.0.1:4000', // jekyll
+        watchOptions : {
+            ignoreInitial : true,
+            ignored : [
+                './_site/node_modules/*',
+                './_site/jekyll/*',
+            ]
+        },
+        online : false,
+        notify : false,
+        ui : false,
+        files : [
+            './_site/**/*',
+        ]
+    });
+});
---
+++ index.md
@@ -0,0 +1,12 @@
+---
+layout: default
+---
+ 
+A happy, healthy dog is an active dog - and Bark Life can help your Hali-pup get moving! We offer individual or small group walks and puppy drop-in services seven days a week, and can customize a plan that works for your dog's age, fitness and skill set.
+ 
+Bark Life is…
+
+- Insured
+- Experienced
+- Pet First Aid certified
+- Any-weather ready
